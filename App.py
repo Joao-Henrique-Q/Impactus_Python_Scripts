7682060ba9c58a3aa9b1cee8a7636b0dbbc77157
Jaumzinho109 <130465683+Jaumzinho109@users.noreply.github.com>
2025-04-14 22:16:37 -0300

    acabei de jogar os gráficos no dashboard

--- App.py
+++ App.py
@@ -55,11 +55,14 @@ if menu == "Inflação":
         )
 
        
-        plt.close("all")
+        
 
         if opcao_grafico == "PCE Contributions":
+            
             st.pyplot(pce_decomposto)
+            plt.close("all")
         elif opcao_grafico == "SA Main MoM %":
+            
             st.pyplot(pce_core_mom)
             st.pyplot(pce_serv_mom)
             st.pyplot(pce_goods_mom)
@@ -67,8 +70,10 @@ if menu == "Inflação":
             st.pyplot(pce_ndur_mom)
             st.pyplot(pce_food_mom)
             st.pyplot(pce_en_mom)
+            plt.close("all")
             
         elif opcao_grafico == "SA Main YoY %":
+            
             st.pyplot(pce_core_ya)
             st.pyplot(pce_serv_ya)
             st.pyplot(pce_goods_ya)
@@ -76,6 +81,7 @@ if menu == "Inflação":
             st.pyplot(pce_durable_yoy)
             st.pyplot(pce_food_yoy)
             st.pyplot(pce_en_ya)
+            plt.close("all")
             
         
             
@@ -86,9 +92,9 @@ if menu == "Inflação":
             "Selecione a Visualização",
             ["NSA - Main", "SA Main MoM %", "SA Main YoY %"]
         )
-        plt.close("all")
+        
         if opcao_grafico == "NSA - Main":
-          
+            
             st.pyplot(core_cpi_nsa)
             st.pyplot(cpi_head_nsa)
             st.pyplot(core_goods_nsa)
@@ -96,41 +102,51 @@ if menu == "Inflação":
             st.pyplot(core_less_shelter_cars_trucks)
             st.pyplot(services_less_shelter)
             st.pyplot(services_less_med)
+            plt.close("all")
         elif opcao_grafico == "SA Main MoM %":
+            
             st.pyplot(graf_sa_core)
             st.pyplot(graf_sa_cpi)
             st.pyplot(graf_sa_core_goods)
             st.pyplot(graf_sa_core_services)
             st.pyplot(graf_sa_food)
             st.pyplot(graf_sa_energy)
+            plt.close("all")
         elif opcao_grafico == "SA Main YoY %":
+            
             st.pyplot(graf_sa_ya_core)
             st.pyplot(graf_sa_ya_head)
             st.pyplot(graf_sa_ya_cgoods)
             st.pyplot(graf_sa_ya_cservices)
             st.pyplot(graf_sa_ya_food)
             st.pyplot(graf_sa_ya_energy)
+            plt.close("all")
 
     elif subtema == "PPI":
         opcao_grafico = st.selectbox(
             "Selecione a Visualização",
             ["Mom %", "YoY %"]
         )
-        plt.close("all")
+        
         if opcao_grafico == "Mom %":
+            
             st.pyplot(graf_core_ppi_mom)
             st.pyplot(graf_airline_passangers_mom)
             st.pyplot(graf_hospital_inpatient_care_mom)
             st.pyplot(graf_hospital_outpatient_care_mom)
             st.pyplot(graf_hospital_physician_care_mom)
             st.pyplot(graf_nursing_home_care_mom)
+            plt.close("all")
+
         elif opcao_grafico == "YoY %":
+            
             st.pyplot(graf_core_ppi_yoy)
             st.pyplot(graf_airline_passangers_yoy)
             st.pyplot(graf_hospital_inpatient_care_yoy)
             st.pyplot(graf_hospital_outpatient_care_yoy)
             st.pyplot(graf_hospital_physician_care_yoy)
             st.pyplot(graf_nursing_home_care_yoy)
+            plt.close("all")
 
 
 
@@ -152,45 +168,59 @@ elif menu == "Mercado de Trabalho":
 
     if subtema_trabalho == "Emprego":
 
-        plt.close("all")  
+        
         st.pyplot(unrate)
         st.pyplot(beveridge_curve)
         st.pyplot(beveridge_ratio)
         st.pyplot(participation_rate)
         st.pyplot(employment_change)
         st.pyplot(sahm_rule)
+        plt.close("all")  
 
     if subtema_trabalho == "Payroll":
         
 
-        plt.close("all")  
+         
         st.pyplot(payroll)
         st.pyplot(private_vs_government)
         st.pyplot(cic_payroll)
         st.pyplot(goods_vs_services_payroll)
         st.pyplot(ordering)
         st.pyplot(breakdown_payroll)
+        plt.close("all") 
+
+
     if subtema_trabalho == "Salários":
-        plt.close("all")  
+          
         st.pyplot(average_hourly_earnings_mom)
         st.pyplot(average_hourly_earnings_yoy)
         st.pyplot(labor_cost)
+        plt.close("all") 
 
 elif menu == 'Política Monetária e Juros':
     subtema_pm = option_menu(
         menu_title=None,
-        options=["Juros de Títulos Públicos"],
+        options=["Juros de Títulos Públicos", "Dados de Política Monetária"],
         default_index=0,
         orientation="horizontal"
     )
     if subtema_pm == 'Juros de Títulos Públicos':
+        
         st.pyplot(graf_3m)
         st.pyplot(graf_10yr)
         st.pyplot(graf_7yr)
         st.pyplot(graf_20yr)
         st.pyplot(graf_30yr)
-
-
+        plt.close("all")
+    if subtema_pm == "Dados de Política Monetária":
+        
+        st.pyplot(graf_ffr)
+        st.pyplot(graf_dif_r)
+        st.pyplot(graf_ta)
+        st.pyplot(graf_repo)
+        st.pyplot(graf_tga)
+        st.pyplot(graf_mv)
+        plt.close("all")
 
 elif menu == "Atividade Econômica":
     subtema_atividade = option_menu(
@@ -201,7 +231,7 @@ elif menu == "Atividade Econômica":
     )
     if subtema_atividade == "Renda":
         
-        plt.close("all")  
+         
         st.pyplot(graf_pi)
         st.pyplot(graf_rdi)
         st.pyplot(graf_dividends)
@@ -209,8 +239,10 @@ elif menu == "Atividade Econômica":
         st.pyplot(graf_rent_income)
         st.pyplot(graf_personal_income_interest)
         st.pyplot(graf_personal_dividend_income)
+        plt.close("all") 
+
     if subtema_atividade == "Consumo":
-        plt.close("all")
+        
         st.pyplot(graf_personal_outlays)
         st.pyplot(graf_real_personal_consumption_expenditures)
         st.pyplot(graf_personal_saving_rate)
@@ -218,16 +250,18 @@ elif menu == "Atividade Econômica":
         st.pyplot(graf_real_personal_consumption_expenditures_goods)
         st.pyplot(graf_real_personal_consumption_expenditures_durables_goods)
         st.pyplot(graf_real_personal_consumption_expenditures_nondurables_goods)
-    
-    if subtema_atividade == "Vendas no Varejo":
         plt.close("all")
+
+    if subtema_atividade == "Vendas no Varejo":
+        
         st.pyplot(graf_retail_sales)
         st.pyplot(graf_retail_sales_excl_motor_vehicle)
         st.pyplot(graf_real_retail_sales)
         st.pyplot(graf_retail_sales_yoy)
+        plt.close("all")
 
     if subtema_atividade == "PIB":
-        plt.close("all")
+        
         st.pyplot(graf_output_gap)
         st.pyplot(graf_yoy_gov_and_inv)
         st.pyplot(graf_real_gross_domestic_product)
@@ -241,6 +275,7 @@ elif menu == "Atividade Econômica":
         st.pyplot(graf_federal_government_consumption_expenditures_interest_payments)
         st.pyplot(graf_government_national_defense_consumption)
         st.pyplot(graf_national_nondefense_consumption)
+        plt.close("all")
 
 
 
--- juros_e_pm.py
+++ juros_e_pm.py
@@ -67,3 +67,135 @@ r30 = fred.get_series("DGS30")
 yield30 = pd.DataFrame()
 yield30["Juros"] = pd.DataFrame(r30/100)
 graf_30yr = plot_juros(yield30, "30 Year Treasury Rate")
+
+
+
+
+#Estatística de Banco Central
+
+
+def graf_com_recessao(df, suptitle="Defina o Suptitle", title="Defina o Título"):
+    plt.close("all")
+    graf, ax = plt.subplots(figsize=(12,5))
+
+    primeira_coluna = df.columns[0]
+
+    ax.plot(df.index, df[primeira_coluna], color="#082631", lw=2, label=suptitle)
+    ax.fill_between(recessao_mensal.index, 0, 1, where=recessao_mensal["USRECD"] == 1, color='gray', alpha=0.3, transform=ax.get_xaxis_transform(), label='Recession Periods')
+    ax.set_title(title, fontsize=8, style='italic', pad=10)
+    graf.suptitle(suptitle, fontsize=15, fontweight='bold')
+    ax.legend(loc="upper left", fontsize=8, frameon=False)
+    ax.spines["top"].set_visible(False)
+    ax.spines["right"].set_visible(False)
+    ax.spines["left"].set_color('#d9d9d9')
+    ax.spines["bottom"].set_color('#d9d9d9')
+
+    ultimo_valor = df[primeira_coluna].iloc[-1]
+    ultimo_indice = df.index[-1]
+    ax.text(ultimo_indice, ultimo_valor,
+        f'{ultimo_valor:.2%}',
+        fontsize=10, fontweight='bold',
+        color='#082631', ha='left', va='center')
+
+    ax.axhline(0, color='black', lw=0.8)
+    ax.set_xlabel("Fonte: FRED | Impactus UFRJ", fontsize=8, labelpad=15)
+    plt.tight_layout()
+    return graf
+
+ffr = fred.get_series("DFF")
+federal_funds_rate = pd.DataFrame()
+federal_funds_rate["Effective rates"] = pd.DataFrame(ffr/100)
+federal_funds_rate = federal_funds_rate.resample("MS").first()
+federal_funds_rate = federal_funds_rate.tail(850)
+r = fred.get_series("USRECD")
+recessions = pd.DataFrame(r, columns=["USRECD"])
+recessao_mensal = recessions.resample('MS').first()
+recessao_mensal = recessao_mensal.tail(850)
+graf_ffr = graf_com_recessao(federal_funds_rate, suptitle= "Federal Funds Effective Rates", title= 'Percent')
+
+
+r103m = fred.get_series("T10Y3M")
+tenyr_minus_threemonths= pd.DataFrame()
+tenyr_minus_threemonths["10-Year Treasury Minus 3-Month Treasury"] = pd.DataFrame(r103m/100)
+tenyr_minus_threemonths = tenyr_minus_threemonths.resample("W").first()
+r = fred.get_series("USRECD")
+recessions = pd.DataFrame(r, columns=["USRECD"])
+recessao_mensal = recessions.resample("W").first()
+recessao_mensal = recessao_mensal.tail(len(tenyr_minus_threemonths["10-Year Treasury Minus 3-Month Treasury"]))
+graf_dif_r = graf_com_recessao(tenyr_minus_threemonths, suptitle= "10-Year Treasury Minus 3-Month Treasury",title="Percent")
+
+
+sr = fred.get_series("SOFR")
+sofr = pd.DataFrame(sr/100, columns=["SOFR"])
+sofr = sofr.resample("W").first()
+r = fred.get_series("USRECD")
+recessions = pd.DataFrame(r, columns=["USRECD"])
+recessao_mensal = recessions.resample("W").first()
+recessao_mensal = recessao_mensal.tail(len(sofr["SOFR"]))
+graf_sofr = graf_com_recessao(sofr, suptitle="SOFR", title="Percent")
+
+
+def graf_com_recessao_sem_pct(df, suptitle="Defina o Suptitle", title="Defina o Título"):
+    plt.close("all")
+    graf, ax = plt.subplots(figsize=(12,5))
+
+    primeira_coluna = df.columns[0]
+
+    ax.plot(df.index, df[primeira_coluna], color="#082631", lw=2, label=suptitle)
+    ax.fill_between(recessao_mensal.index, 0, 1, where=recessao_mensal["USRECD"] == 1, color='gray', alpha=0.3, transform=ax.get_xaxis_transform(), label='Recession Periods')
+    ax.set_title(title, fontsize=8, style='italic', pad=10)
+    graf.suptitle(suptitle, fontsize=15, fontweight='bold')
+    ax.legend(loc="upper left", fontsize=8, frameon=False)
+    ax.spines["top"].set_visible(False)
+    ax.spines["right"].set_visible(False)
+    ax.spines["left"].set_color('#d9d9d9')
+    ax.spines["bottom"].set_color('#d9d9d9')
+
+    ultimo_valor = df[primeira_coluna].iloc[-1]
+    ultimo_indice = df.index[-1]
+
+  
+    ax.set_xlabel("Fonte: FRED | Impactus UFRJ", fontsize=8, labelpad=15)
+    plt.tight_layout()
+    return graf
+
+ta = fred.get_series("WALCL")
+fed_total_assets = pd.DataFrame(ta, columns=["Total Assets"])
+fed_total_assets = fed_total_assets.resample("W").first()
+r = fred.get_series("USRECD")
+recessions = pd.DataFrame(r, columns=["USRECD"])
+recessao_mensal = recessions.resample("W").first()
+recessao_mensal = recessao_mensal.tail(len(fed_total_assets["Total Assets"]))
+graf_ta = graf_com_recessao_sem_pct(fed_total_assets, suptitle="FED's Total Assets", title="Millions of Dollars")
+
+
+repo = fred.get_series("RRPONTSYD")
+overnight_repo_facilities = pd.DataFrame(repo, columns=["Overnight Reverse Repurchase Agreements"])
+overnight_repo_facilities = overnight_repo_facilities.resample("W").first()
+overnight_repo_facilities = overnight_repo_facilities.tail(500)
+r = fred.get_series("USRECD")
+recessions = pd.DataFrame(r, columns=["USRECD"])
+recessao_mensal = recessions.resample("W").first()
+recessao_mensal = recessao_mensal.tail(len(overnight_repo_facilities["Overnight Reverse Repurchase Agreements"]))
+graf_repo = graf_com_recessao_sem_pct(overnight_repo_facilities, suptitle="Overnight Reverse Repurchase Agreements", title="Billions of Dollars")
+
+
+tga = fred.get_series("WDTGAL")
+tga_df = pd.DataFrame(tga, columns=["TGA Balance"])
+r = fred.get_series("USRECD")
+recessions = pd.DataFrame(r, columns=["USRECD"])
+recessao_mensal = recessions.resample("W").first()
+recessao_mensal = recessao_mensal.tail(len(tga_df["TGA Balance"]))
+graf_tga = graf_com_recessao_sem_pct(tga_df, suptitle="Treasury General Account (TGA)", title="Weekly US Treasury balance at the Fed (in billions of dollars)")
+
+
+mv = fred.get_series("M1V")
+money_velocity = pd.DataFrame(mv, columns=["Ratio"])
+r = fred.get_series("USRECD")
+recessions = pd.DataFrame(r, columns=["USRECD"])
+recessao_mensal = recessions.resample("QE").first()
+recessao_mensal = recessao_mensal.tail(len(money_velocity["Ratio"]))
+graf_mv = graf_com_recessao_sem_pct(money_velocity, suptitle="Velocity of M1 Money Stock", title="Ratio")
+
+
+
